ebf00085fb05c2a3abc2d89ba1cc149715f0b515
toivyb <toivybraun@gmail.com>
2025-08-31 00:26:36 -0400

    fly buildpacks: use wsgi:app and correct Procfile encoding

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,7 @@ import sys
 # Add the current directory to Python path
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-from estatecore_backend import create_app
+from __init__ import create_app
 
 app = create_app()
 
